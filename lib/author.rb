5552fe1aa99a8d0b207c041b3ca46419166e3665
John Stassen <john.stassen@gmail.com>
2020-08-28 01:44:07 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -24,8 +24,8 @@ class Author
   end
 
   def add_post_by_title(title)
-    pong = Post.new(title)
-    add_pong(pong)
+    post = Post.new(title)
+    add_post(post)
   end
 
   def self.post_count
